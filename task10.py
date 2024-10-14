b377425f8556bdf2e8b28cb53b7c1ea612bc28ba
Егорова Лилиана Сергеевна <l.egorova@main.nsu.ru>
2024-10-14 14:09:05 +0700

    еще 3 задания

--- task10.py
+++ task10.py
@@ -6,7 +6,7 @@ e = abs(a - c)
 f = abs(b - d)
 if (a > 8 or b > 8 or c > 8 or d > 8) or (a < 1 or b < 1 or c < 1 or d < 1):
     print ('Неверный номер столбца или строки')
-elif e == f:
+elif (e == f) or (a == c) or (b == d):
     print('YES')
 else:
     print('NO')
--- task9.py
+++ task9.py
@@ -2,11 +2,11 @@ a = int(input())
 b = int(input())
 c = int(input())
 d = int(input())
-e = abs(a - c)
-f = abs(b - d)
 if (a > 8 or b > 8 or c > 8 or d > 8) or (a < 1 or b < 1 or c < 1 or d < 1):
     print ('Неверный номер столбца или строки')
-elif e == f:
+elif (a == c + 1 or a == c - 1) and (b == d + 2 or b == d - 2):
+    print ('YES')
+elif (a == c + 2 or a == c - 2) and (b == d + 1 or b == d - 1):
     print('YES')
 else:
-    print('NO')
+    print ('NO')
